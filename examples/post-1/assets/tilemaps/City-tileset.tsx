30c76037e8f17ee8fb729b1972cd040e2e12776a
OdinScriptKitty <trevinator001@gmail.com>
2022-04-26 19:53:11 -0600

    New File Locations

---
+++ examples/post-1/assets/tilemaps/City-tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="roguelikeCity_transparent" tilewidth="16" tileheight="16" margin="1" tilecount="1131" columns="39">
+ <image source="../tilesets/City-Terrainset/Spritesheet/roguelikeCity_transparent.png" trans="ff00ff" width="628" height="475"/>
+</tileset>
---
+++ examples/post-1/assets/tilemaps/tilemap_packed.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="tilemap_packed" tilewidth="16" tileheight="16" margin="1" tilecount="442" columns="26">
+ <image source="../../../../../../pokeMon/Tile-City-Streets/Tilemap/tilemap_packed.png" width="432" height="288"/>
+</tileset>
